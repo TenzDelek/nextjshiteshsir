514f9e6ad96a1123d5324aafd60b478d61126745
TenzDelek <tibetdelek@gmail.com>
2024-03-22 21:37:39 +0530

    created the signup backend and frontend

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,58 @@
+//importing connect to handshake with database
+import { connect } from "@/dbConfig/dbConfig";
+//importing the structure model to put data
+import User from "@/models/userModel";
+//request and response
+import { NextRequest, NextResponse } from "next/server";
+//encrypting password
+import bcryptjs from "bcryptjs";
+
+connect();
+//breaking down the overall step to be done in signup
+//1.first get the details from the frontend from either url or the body(here we got it from the body)
+//2.then destructure the data we got
+//3.now the validation, storing and encryptions takes place
+//4.here first we check if the user is already signed up 
+//5.then we encrpyt the password
+//6.now we store the data in the database (the data that we got from the frontend)
+//7.send the response back
+//handling the post request
+export async function POST(request: NextRequest) {
+  try {
+    //grabing data from the body (in express we do req.body)
+    const reqBody = await request.json(); //not express but nextserver
+    //now we destructure the reqbody(we know that we will get username,password and email)
+    const { username, email, password } = reqBody;
+    console.log(reqBody);
+    //1.basic validation now (the funadamental of backend remember)
+    //check if the user already exist
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exist cho" },
+        { status: 400 }
+      );
+    }
+
+    //now hashing(encrypting the password)
+    //Instead of simply hashing the given password, bcrypt
+    //adds a random piece of data, called salt,
+    const salt = await bcryptjs.genSalt(10); //here the 10 is the rounds(depends like express and mongoose use 10, ruby on rail use 12)
+    const hashedPassword = await bcryptjs.hash(password, salt); //remember the password comes from the body
+
+    //now saving the user in the database (the input will be coming the form that we created, and through the request body we got the info)
+   const newUser= new User({ username, email, password: hashedPassword });
+    //dont forget to save
+    const savedUser=await newUser.save()
+    console.log(savedUser)
+
+    //now send a response back
+    return NextResponse.json({
+        message:"User created yes sir",
+        success:true,
+        savedUser //send the detail
+    })
+} catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,24 +1,49 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
-// import {axios} from "axios"
+import { useEffect, useState } from "react";
+import axios from "axios"
+import toast from "react-hot-toast";
 const Signup = () => {
+  const router=useRouter()
   const [user, setuser] = useState({
     email: "",
     password: "",
     username: "",
   });
+
+  const [buttondisabled,setbuttondisabled]=useState(false) //just to make sure every field is filled
+  useEffect(()=>{
+    if(user.email.length>0 && user.password.length>0 && user.username.length>0)
+    {
+      setbuttondisabled(false)
+    }
+    else{
+      setbuttondisabled(true)
+    }
+  },[user])//depends on user
+  const[loading,setloading]=useState(false)
   const onsignup = async () => {
     // async as it is going to talk with database
+    try {
+      setloading(true)
+      const res=await axios.post("/api/users/signup",user)//hit the url and also taking along user which are the input datas
+      console.log("signup success",res.data)
+      router.push("/login") //push to login if the signup button is clicked
+    } catch (error:any) {
+      console.log("signup failed",error.message)
+      toast.error(error.message)
+    }finally{
+      setloading(false)
+    }
   };
   return (
     <div className=" flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Signup</h1>
+      <h1>{loading?"PROCESSING":"SIGNUP"}</h1>
       <hr />
       <label htmlFor="username">username</label>
       <input 
-      className=" p-2 border border-gray-300 rounded-lg 
+      className=" p-2 border border-gray-300 rounded-lg  text-black
        mb-4 focus:outline-none focus:border-gray-600"
         type="text"
         id="username"
@@ -29,7 +54,7 @@ const Signup = () => {
       />
       <label htmlFor="email">email</label>
       <input 
-      className=" p-2 border border-gray-300 rounded-lg 
+      className=" p-2 border border-gray-300 rounded-lg text-black
        mb-4 focus:outline-none focus:border-gray-600"
         type="text"
         id="email"
@@ -40,7 +65,7 @@ const Signup = () => {
       />
       <label htmlFor="password">password</label>
       <input 
-      className=" p-2 border border-gray-300 rounded-lg 
+      className=" p-2 border border-gray-300 rounded-lg text-black
        mb-4 focus:outline-none focus:border-gray-600"
         type="text"
         id="password"
@@ -52,7 +77,7 @@ const Signup = () => {
       <button
       onClick={onsignup}
       className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
-        Signup
+        {buttondisabled?"notyet":"signup now"}
       </button>
       <Link href="/login">Visit Login</Link>
     </div>
